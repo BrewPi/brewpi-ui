5c6c71baedccf2c1e672923a2ed8da7b085cff9b
Elco Jacobs <elcojacobs@gmail.com>
2016-09-27 13:29:24 +0200

    added custom flow arrows for bridge

--- app/components/FlowArrows/index.js
+++ app/components/FlowArrows/index.js
@@ -18,16 +18,30 @@ const SvgArrows = {
   E: <polyline key="E" points="35.5 21 39.5 25 35.5 29" />,
 };
 
+const SvgArrowsBridge = {
+  s: <polyline key="s" points="29 39.5 25 35.5 21 39.5" />,
+  S: <polyline key="S" points="29 35.5 25 39.5 21 35.5" />,
+  e: <polyline key="e" points="43.5 21 39.5 25 43.5 29" />,
+  E: <polyline key="E" points="39.5 21 43.5 25 39.5 29" />,
+  W: <polyline key="w" points="10.5 21 6.5 25 10.5 29" />,
+  w: <polyline key="W" points="6.5 21 10.5 25 6.5 29" />,
+};
+
+function pickArrows(flow, arrows) {
+  const picked = [];
+  for (const ch of flow) {
+    picked.push(arrows[ch]);
+  }
+  return picked;
+}
+
 /* Renders flow arrows, to be used inside an svg tag
  * flow is a string containing the letters NESW.
  * lowercase is inflow, uppercase is outflow
  */
 export const FlowArrows = (props) => {
   const flow = props.flow || '';
-  const arrows = [];
-  for (const ch of flow) {
-    arrows.push(SvgArrows[ch]);
-  }
+  const arrows = pickArrows(flow, SvgArrows);
   return (
     <g className={styles.flowArrows}>
       {arrows}
@@ -38,5 +52,24 @@ FlowArrows.propTypes = {
   flow: React.PropTypes.string,
 };
 
-export default FlowArrows;
+/* Renders flow arrows, to be used inside an svg tag
+ * flow is a string containing the letters NESW.
+ * lowercase is inflow, uppercase is outflow
+ */
+export const FlowArrowsBridge = (props) => {
+  const flow = props.flow || '';
+  const arrows = pickArrows(flow, SvgArrowsBridge);
+  return (
+    <g className={styles.flowArrows}>
+      {arrows}
+    </g>
+  );
+};
+FlowArrowsBridge.propTypes = {
+  flow: React.PropTypes.string,
+};
 
+export default {
+  FlowArrowsBridge,
+  FlowArrows,
+};
