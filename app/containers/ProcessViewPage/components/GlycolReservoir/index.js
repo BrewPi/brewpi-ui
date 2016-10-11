20af181d4c92b89085227896c9706af7e5a93951
Joep Klunhaar <joepk1989@gmail.com>
2016-10-11 17:37:38 +0200

    resized glycol reservoir

--- app/containers/ProcessViewPage/components/GlycolReservoir/index.js
+++ app/containers/ProcessViewPage/components/GlycolReservoir/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import styles from './styles.css';
-const classNames = require('classnames');
 
 const SvgGlycolReservoir = require('./svg/glycol_reservoir.svg?tag=g');
-const SvgIconCooling = require('../Icons/svg/cooling.svg');
 
 import { SvgParent } from '../SvgParent';
 
-export const GlycolReservoir = (props) => (
+export const GlycolReservoir = () => (
   <SvgParent viewBox={'0 0 100 150'}>
     <SvgGlycolReservoir className={styles.glycolReservoir} />
-    <SvgIconCooling className={classNames(styles.iconCooling)} />
   </SvgParent>
 );
 GlycolReservoir.propTypes = {
