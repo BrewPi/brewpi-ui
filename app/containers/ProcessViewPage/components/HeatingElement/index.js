caf3027a72097d86a5c7ba05fa8594f0a2b8466f
Elco Jacobs <elcojacobs@gmail.com>
2016-10-13 17:32:48 +0200

    unflip text value in heating element display when the part is flipped. Converted it to a span instead of text inside svg

--- app/containers/ProcessViewPage/components/HeatingElement/index.js
+++ app/containers/ProcessViewPage/components/HeatingElement/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import styles from './styles.css';
 import { Liquids } from '../Liquids';
+const classNames = require('classnames');
 
 const SvgElement = require('./svg/heater_element.svg?tag=g');
 const SvgDisplay = require('./svg/heater_display.svg?tag=g');
-const SvgValue = require('./svg/value.svg?tag=g');
 
 import { SvgParent } from '../SvgParent';
 
-export const HeatingElement = (props) => (
-  <SvgParent viewBox={'0 0 250 50'}>
-    <SvgElement className={styles.element} />
-    <SvgDisplay className={styles.display} style={Liquids.fillStyle(props.liquid)} />
-    <SvgValue className={styles.value} />
-  </SvgParent>
-);
+export const HeatingElement = (props) => {
+  const { flip } = props; // is parent flipped?
+  const unflipStyle = flip ? styles.unflip : undefined;
+  return (
+    <div className={styles.root}>
+      <SvgParent viewBox={'0 0 250 50'}>
+        <SvgElement className={styles.element} />
+        <SvgDisplay className={styles.display} style={Liquids.fillStyle(props.liquid)} />
+      </SvgParent>
+      <span className={classNames(styles.value, unflipStyle)}>0%</span>
+    </div>
+  );
+};
 HeatingElement.propTypes = {
+  flip: React.PropTypes.bool,
   liquid: React.PropTypes.string,
 };
