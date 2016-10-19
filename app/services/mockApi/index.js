f71761193efdadddd101bc46c1c348d746aa5e12
Elco Jacobs <elco@brewpi.com>
2016-10-19 20:45:33 +0200

    changed mock api directory structure

--- app/services/mockApi/index.js
+++ app/services/mockApi/index.js
@@ -1,36 +1,19 @@
+/* eslint-disable global-require */
 import { fromJS } from 'immutable';
+import herms from './sample-data/herms';
+import fermentation from './sample-data/fermentation';
 
-const hermsView = require('./sample-data/view-herms.json');
-const hermsLayout = require('./sample-data/layout-herms.json');
-const step1 = require('./sample-data/steps/1_valves_closed.json');
-const step2 = require('./sample-data/steps/2_valves_open.json');
-hermsView.layouts = { 0: hermsLayout };
-hermsView.steps = [
-  step1,
-  step2,
-];
+let views = {
+  herms,
+  fermentation,
+  view1: require('./sample-data/test_data/view1.json'),
+  view2: require('./sample-data/test_data/view2.json'),
+  view3: require('./sample-data/test_data/view3.json'),
+  view4: require('./sample-data/test_data/view4.json'),
+  coilTest: require('./sample-data/test_data/coil_test.json'),
+};
 
-
-const fermentView = require('./sample-data/view-ferment.json');
-const fermentLayout = require('./sample-data/layout-ferment.json');
-fermentView.layouts = { 0: fermentLayout };
-
-const view1 = require('./sample-data/test_data/view1.json');
-const view2 = require('./sample-data/test_data/view2.json');
-const view3 = require('./sample-data/test_data/view3.json');
-const view4 = require('./sample-data/test_data/view4.json');
-const coilTest = require('./sample-data/test_data/coil_test.json');
-
-const views = fromJS({
-  herms: hermsView,
-  fermentation: fermentView,
-  view1,
-  view2,
-  view3,
-  view4,
-  coilTest,
-});
-
+views = fromJS(views);
 
 export const api = {
   getProcessViews() {
---
+++ app/services/mockApi/sample-data/herms/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable global-require */
+const view = require('./view.json');
+
+view.layouts = {
+  0: require('./layout.json'),
+};
+view.steps = [
+  require('./steps/1_valves_closed.json'),
+  require('./steps/2_valves_open.json'),
+  require('./steps/3_fill_hlt.json'),
+  require('./steps/4_fill_bk.json'),
+];
+
+export default view;
