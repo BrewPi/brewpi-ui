f3f2d486446ba3852c892775b997b577a17ccf6a
Elco Jacobs <elcojacobs@gmail.com>
2016-10-21 13:54:45 +0200

    render liquid and rotation in pump

--- app/containers/ProcessViewPage/components/Pump/index.js
+++ app/containers/ProcessViewPage/components/Pump/index.js
@@ -10,27 +10,37 @@ const SvgBlades = require('./svg/blades.svg?tag=g');
 const SvgLiquidPump = require('./svg/liquid_pump.svg?tag=g');
 
 import { SvgParent } from '../SvgParent';
-
+import { pickLiquid } from '../Flows';
 
 export class Pump extends React.Component {
   static flows = () => ({ l: 'r' });
   render() {
-    const rotateClass = getRotateClass(this.props.powered);
+    const powered = this.props.settings.powered;
+    const rotateClass = getRotateClass(powered);
+    const liquid = pickLiquid(this.props.flows, Pump.flows());
     return (
-      <SvgParent>
-        <SvgBall className={styles.ball} style={Liquids.fillStyle(this.props.liquid)} />
-        <g className={styles.bladesWrapper}>
-          <SvgBlades className={classNames(styles.blades, rotateClass)} />
-        </g>
-        <SvgLiquidPump className={styles.liquid} style={Liquids.strokeStyle(this.props.liquid)} />
-        <SvgTubes className={classNames(styles.tubes)} />
-      </SvgParent>
+      <div>
+        <SvgParent>
+          <SvgBall className={styles.ball} style={Liquids.fillStyle(liquid)} />
+          <g className={styles.bladesWrapper}>
+            <SvgBlades className={classNames(styles.blades, rotateClass)} />
+          </g>
+          <SvgLiquidPump className={styles.liquid} style={Liquids.strokeStyle(liquid)} />
+          <SvgTubes className={classNames(styles.tubes)} />
+        </SvgParent>
+        <span className={styles.id}>{this.props.id}</span>
+      </div>
     );
   }
 }
 Pump.propTypes = {
-  powered: React.PropTypes.string,
-  liquid: React.PropTypes.string,
+  settings: React.PropTypes.object,
+  flows: React.PropTypes.array,
+  id: React.PropTypes.string,
 };
+Pump.defaultProps = {
+  settings: new Map(),
+};
+
 
 export default Pump;
