69ce157109b4329040a27068d2b65a886bc284d3
Elco Jacobs <elcojacobs@gmail.com>
2016-10-21 10:55:53 +0200

    Added a prime pumps step

--- app/services/mockApi/sample-data/herms/index.js
+++ app/services/mockApi/sample-data/herms/index.js
@@ -9,13 +9,14 @@ view.steps = [
   require('./steps/02_valves_open.json'),
   require('./steps/03_fill_hlt.json'),
   require('./steps/04_fill_bk.json'),
-  require('./steps/05_heat_hlt_and_bk.json'),
-  require('./steps/06_add_grain.json'),
-  require('./steps/07_mash_in.json'),
-  require('./steps/08_mash_rest.json'),
-  require('./steps/09_mash_recirc.json'),
-  require('./steps/10_mash_out.json'),
-  require('./steps/11_fly_sparge.json'),
+  require('./steps/05_prime_pumps.json'),
+  require('./steps/06_heat_hlt_and_bk.json'),
+  require('./steps/07_add_grain.json'),
+  require('./steps/08_mash_in.json'),
+  require('./steps/09_mash_rest.json'),
+  require('./steps/10_mash_recirc.json'),
+  require('./steps/11_mash_out.json'),
+  require('./steps/12_fly_sparge.json'),
 ];
 
 export default view;
