de1fa18589bfc2307922dc7e6e1a2cb9a19fb7e9
Joep Klunhaar <joepk1989@gmail.com>
2016-12-20 17:48:27 +0100

    WIP BIAB process steps

--- app/services/mockApi/sample-data/rims-biab/index.js
+++ app/services/mockApi/sample-data/rims-biab/index.js
@@ -2,14 +2,16 @@
 const view = require('./view.json');
 
 view.layouts = [
-  require('./layouts/00_idle.json'),
-  require('./layouts/01_fill_hlt.json'),
-  require('./layouts/02_fill_bk.json'),
+  require('./layouts/00_fill_kettle.json'),
+  require('./layouts/01_preheat_kettle.json'),
+  require('./layouts/02_connect_rims_tube.json'),
+  require('./layouts/03_add_grain.json'),
 ];
 view.steps = [
-  require('./steps/00_idle.json'),
-  require('./steps/01_fill_hlt.json'),
-  require('./steps/02_fill_bk.json'),
+  require('./steps/00_fill_kettle.json'),
+  require('./steps/01_preheat_kettle.json'),
+  require('./steps/02_connect_rims_tube.json'),
+  require('./steps/03_add_grain.json'),
 ];
 
 export default view;
